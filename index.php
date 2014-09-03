f42e49411d71ab3fe992232216a034efce749103
Chris <chris@wolchesky.net>
2014-09-03 15:22:47 -0400

    Initial content & Commit.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Chris Wolchesky</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css" media="all" />
+	</head>
+	<body>
+		<div id="page">
+			<div id="content_container">
+				<div id="content">
+					<h1>Chris Wolchesky</h1>
+					<h4>Nassau County, Long Island, NY</h4>
+					<h4>PHP &amp; Java Developer</h4>
+					<ul>
+						<li><a href="http://github.com/cwolchesky/">GitHub</a></li>
+						<li><a href="resume.php">R&eacute;sum&eacute;</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
